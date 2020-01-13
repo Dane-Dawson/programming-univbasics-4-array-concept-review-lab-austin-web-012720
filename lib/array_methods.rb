f20d9838d7c5731c68dfb424f7992a0905d37d1c
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 19:43:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   counter = 0
   while array[counter] do
     break if array[counter] == value_to_find
-    counter =+ 1
+    counter += 1
   end
   return counter + 1
 end
