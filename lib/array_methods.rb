64a370ba9c3e1deb21eea3f81a63d2ddb196192d
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 19:41:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,30 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array[counter] do
+    break if array[counter] == value_to_find
+    counter =+ 1
+  end
+  return counter + 1
 end
 
 def find_max_value(array)
-  # Add your solution here
+    counter = 0
+    max_value = 0
+  while array[counter] do
+    if (array[counter] > max_value)
+      max_value == array[counter]
+    counter += 1
+  end
+  return max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+    counter = 0
+    min_value = 
+  while array[counter] do
+    if (array[counter] > min_value)
+      min_value == array[counter]
+    counter += 1
+  end
+  return max_value
 end
